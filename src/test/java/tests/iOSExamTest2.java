9f3d494a14fa77983e80e6c56da966a4dd565c4a
Davis Miluss <younglv@Daviss-MacBook-Pro-2.local>
2025-04-07 21:31:23 +0300

    Updated examTest2

--- src/test/java/tests/iOSExamTest2.java
+++ src/test/java/tests/iOSExamTest2.java
@@ -9,8 +9,8 @@ public class iOSExamTest2 extends DriverSetup {
 
     @Test(testName = "iOS Exam Test 2")
     public void iOSExamTest2(){
-        //Assert.assertTrue(splashScreen.splashScreenLoaded(), "Splash Screen not Loaded");
-        //splashScreen.clickContinueButton();
+        Assert.assertTrue(splashScreen.splashScreenLoaded(), "Splash Screen not Loaded");
+        splashScreen.clickContinueButton();
         Assert.assertTrue(calendarHomeScreen.calendarHomeScreenLoaded(), "Calendar home screen not loaded");
 
         calendarHomeScreen.clickCalendarsButton();
